7bb3a1a865272ef0bc27175e7d18c81120df4a79
Miguel Claudio <74688420+Programador-master@users.noreply.github.com>
2021-12-15 22:26:48 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,19 @@
 var form = document.querySelector('main section.form')
 
 
+const btnForm = document.querySelector('.btnForm')
+const imgForm = document.querySelector('main section.content img.img')
+var form = document.querySelector('main section.form')
+
+btnForm.addEventListener('click', event => {
+    
+    imgForm.classList.toggle('active')
+    form.classList.toggle('active')
+
+    event.preventDefault()
+})
+
+
 const inpQuestion = document.querySelector('form .question')
 
 const number1 = parseInt(Math.random() *9)
