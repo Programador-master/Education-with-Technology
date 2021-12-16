db9903abba7074c4605aa76d17f3bb324e904dfd
Miguel Claudio <74688420+Programador-master@users.noreply.github.com>
2021-12-15 22:26:23 -0300

    Update and rename modal.js to requesteApi.js

---
+++ scripts/requesteApi.js
@@ -0,0 +1,36 @@
+function handleCredentialResponse(response) {
+                      
+    const data = jwt_decode(response.credential)
+
+    profileInfos(data.name, data.email)
+
+}
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id: "384250235525-mdqbtbonug0k5uo7gcc3qf6mubp2ems6.apps.googleusercontent.com",
+    callback: handleCredentialResponse,
+    context:"use",
+    ux_mode:"popup",
+    login_uri:"",
+    auto_select:"true"
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"),
+    { theme: "filled_black",
+     size: "large",
+     shape:"pill",
+     text:"$ {button.text}",
+     logo_alignment:"left" }  // customization attributes
+  );
+  google.accounts.id.prompt(); // also display the One Tap dialog
+}
+
+function profileInfos(name, email) {
+
+    const inpName = document.querySelector("input.name")
+    const inpEmail = document.querySelector("input.email")
+
+    inpName.innerHTML = name
+    inpEmail.innerHTML = email
+
+}
