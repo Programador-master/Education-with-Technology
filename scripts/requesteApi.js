ea8442abafcbe10cfd055ff86208f7a6f534fa6d
Miguel Claudio <74688420+Programador-master@users.noreply.github.com>
2021-12-18 23:53:32 -0300

    Update requesteApi.js

--- scripts/requesteApi.js
+++ scripts/requesteApi.js
@@ -2,7 +2,7 @@ function handleCredentialResponse(response) {
                       
     const data = jwt_decode(response.credential)
 
-    profileInfos(data.name, data.email)
+    loadInfosProfile(data.name, data.email, "google")
 
 }
 window.onload = function () {
@@ -25,12 +25,55 @@ window.onload = function () {
   google.accounts.id.prompt(); // also display the One Tap dialog
 }
 
-function profileInfos(name, email) {
+const btnSubmitGit = document.querySelector('button#btnSubmitGit')
 
-    const inpName = document.querySelector("input.name")
-    const inpEmail = document.querySelector("input.email")
+btnSubmitGit.addEventListener('click', () => {
 
-    inpName.textContent = name
-    inpEmail.innerHTML = email
+  let nameGit = document.querySelector('input#nameGit')
+  let url = `https://api.github.com/users/${nameGit.value}`
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    loadInfosProfile(data.name, data.email, "git")
+  
+  })
+  
+})
+
+
+function loadInfosProfile(name, email, local) {
+
+  const inpName = document.querySelector('input#name')
+  const inpEmail = document.querySelector('input#email')
+
+  if(local == "git") {
+
+    if(name == undefined && email == undefined) {
+
+      alert("Verifique seu nome do GitHub")
+  
+      nameGit.focus()
+  
+    }else if(email == null) {
+  
+      inpName.value = name 
+  
+      inpEmail.focus()
+  
+    }else {
+  
+      inpEmail.value = email
+      inpQuestion.focus()
+    }
+
+  }else if(local == "google") {
+
+    inpName.value = name 
+    inpEmail.value = email
+
+    inpQuestion.focus()
+
+  }
 
 }
